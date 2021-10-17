d64c8a4ca0d282bb78726d07f975d0f647f3d3cf
Matthias Mailänder <matthias@mailaender.name>
2021-10-17 18:46:29 +0200

    Add a basic extension.

---
+++ README.md
@@ -0,0 +1,9 @@
+# OpenRA Visual Studio Code Lua extension
+
+This is an IDE companion for the [OpenRA Lua API](https://docs.openra.net/en/latest/release/lua/) which is used to write mission scripts in [OpenRA](https://www.openra.net), an Open Source real-time strategy game engine for early Westwood games such as Command & Conquer: Red Alert.
+
+## Setup
+
+Execute `npm install` in the terminal.
+Open the Folder with VS Code.
+Press `F5` to launch it.
---
+++ src/extension.ts
@@ -0,0 +1,52 @@
+/*---------------------------------------------------------
+ * Copyright 2021 The OpenRA Developers (see AUTHORS)
+   This file is part of OpenRA, which is free software. It is made
+   available to you under the terms of the GNU General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version. For more
+   information, see COPYING.
+ *--------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	const newScript = vscode.languages.registerCompletionItemProvider('lua', {
+
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+			const snippetCompletion = new vscode.CompletionItem('New OpenRA Lua mission script.');
+			snippetCompletion.insertText = new vscode.SnippetString('WorldLoaded = function()\n\nend\n\nTick = function()\n\nend');
+			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a barebone script with the function the engine will call.");
+
+			const userInterfaceCompletion = new vscode.CompletionItem('UserInterface');
+
+			return [
+				snippetCompletion,
+				userInterfaceCompletion,
+			];
+		}
+	});
+
+	const ui = vscode.languages.registerCompletionItemProvider(
+		'lua',
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				if (!linePrefix.endsWith('UserInterface.')) {
+					return undefined;
+				}
+
+				const setMissionText = new vscode.CompletionItem('SetMissionText', vscode.CompletionItemKind.Method);
+				setMissionText.documentation = "Displays a text message at the top center of the screen.";
+
+				return [
+					setMissionText,
+				];
+			}
+		},
+		'.'
+	);
+
+	context.subscriptions.push(newScript, ui);
+}
