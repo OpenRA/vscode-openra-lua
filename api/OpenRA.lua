f9f1614892e8785f04cc9a169e7113c848d4d054
Matthias Mailänder <matthias@mailaender.name>
2021-10-18 22:34:41 +0200

    Use the Lua language server and EmmyLua annotations instead.

---
+++ api/OpenRA.lua
@@ -0,0 +1,9 @@
+---@class HSLColor
+HSLColor = { Black = "#000000", White = "#FFFFFF", Red = "#FF0000" }
+
+UserInterface = { }
+
+---@param text  string
+---@param color? HSLColor
+---Displays a text message at the top center of the screen.
+function UserInterface.SetMissionText(text, color) end
--- src/extension.ts
+++ src/extension.ts
@@ -11,6 +11,13 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
+	const extensionId = "vscode-openra-lua";
+	const extensionPath = vscode.extensions.getExtension(extensionId)?.extensionPath;
+	const folderPath = extensionPath+"\\"+"api";
+	const config = vscode.workspace.getConfiguration("Lua");
+	config.update('Lua.workspace.library', [ folderPath ] );
+
+
 	const newScript = vscode.languages.registerCompletionItemProvider('lua', {
 
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
@@ -18,50 +25,11 @@ export function activate(context: vscode.ExtensionContext) {
 			snippetCompletion.insertText = new vscode.SnippetString('WorldLoaded = function()\n\nend\n\nTick = function()\n\nend');
 			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a barebone script with the function the engine will call.");
 
-			const userInterfaceCompletion = new vscode.CompletionItem('UserInterface');
-
 			return [
 				snippetCompletion,
-				userInterfaceCompletion,
 			];
 		}
 	});
 
-	const ui = vscode.languages.registerCompletionItemProvider(
-		'lua',
-		{
-			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-
-				const linePrefix = document.lineAt(position).text.substr(0, position.character);
-				if (!linePrefix.endsWith('UserInterface.')) {
-					return undefined;
-				}
-
-				const setMissionText = new vscode.CompletionItem('SetMissionText', vscode.CompletionItemKind.Method);
-				setMissionText.documentation = "Displays a text message at the top center of the screen.";
-
-				return [
-					setMissionText,
-				];
-			}
-		},
-		'.'
-	);
-
-	context.subscriptions.push(newScript, ui);
-
-	const result = new vscode.MarkdownString();
-	result.appendCodeblock("SetMissionText(string text, color = nil)", "lua");
-	result.appendMarkdown("---\nDisplays a text message at the top center of the screen.");
-
-	vscode.languages.registerHoverProvider('lua', {
-		provideHover(document, position) {
-			const hoveredWordPosition = document.getWordRangeAtPosition(position, /[\w.]+/);
-			const hoveredWord = document.getText(hoveredWordPosition);
-			if (hoveredWord == 'UserInterface.SetMissionText')
-				return new vscode.Hover(result);
-		}
-	});
-
-	vscode.workspace.getConfiguration().update('Lua.diagnostics.globals', [ 'UserInterface' ] );
+	context.subscriptions.push(newScript);
 }
