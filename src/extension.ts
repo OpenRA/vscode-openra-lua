5f84d5f44a8303c478fa9088c8d8076fa64c972e
Matthias Mailänder <matthias@mailaender.name>
2021-10-19 12:59:51 +0200

    Simpler header.

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
-/*---------------------------------------------------------
- * Copyright 2021 The OpenRA Developers (see AUTHORS)
-   This file is part of OpenRA, which is free software. It is made
-   available to you under the terms of the GNU General Public License
-   as published by the Free Software Foundation, either version 3 of
-   the License, or (at your option) any later version. For more
-   information, see COPYING.
- *--------------------------------------------------------*/
+/*
+ Copyright 2021 The OpenRA Developers (see AUTHORS)
+ This file is part of OpenRA, which is free software. It is made
+ available to you under the terms of the GNU General Public License
+ as published by the Free Software Foundation, either version 3 of
+ the License, or (at your option) any later version. For more
+ information, see COPYING.
+ */
 
 import * as vscode from 'vscode';
 
